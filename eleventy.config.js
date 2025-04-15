1008bd16dc59dc441dab2e74c9ba613b8e235ae1
jellyfish <actualjellyfish@gmail.com>
2025-04-14 21:33:31 -0400

    conditionally render photos

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,6 @@
+import fs from "node:fs";
+import path from "node:path";
+
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 import pretty from "pretty";
 
@@ -15,6 +18,18 @@ export default function(eleventyConfig) {
             .map((p) => `<p>${p.trim()}</p>\n`)
             .join("")
     })
+
+    eleventyConfig.addAsyncShortcode("renderPhoto", async function (photoSlug, data={}) {
+        let photoPath = `photo/${photoSlug}.jpg`
+        if (fs.existsSync(path.join("./src", photoPath))) {
+          return `<img
+                class="photo"
+                alt="{% block imgalt %}{% endblock %}" 
+                src="/${photoPath}"
+            >`;
+        }
+        return "";
+      });
 
     eleventyConfig.addTransform("format", function (content) {
         if ((this.page.outputPath || "").endsWith(".html")) {
